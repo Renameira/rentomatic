6ad55cee64531f84e8187fe0766a1f4dfb08aeac
Renato Meira <renato.meira@cortex-intelligence.com>
2021-11-14 16:59:43 -0300

    add new tests

--- rentomatic/domain/room.py
+++ rentomatic/domain/room.py
@@ -1,17 +1,18 @@
 import uuid
 import dataclasses
 
+
 @dataclasses.dataclass
 class Room:
     code: uuid.UUID
-    size : int 
-    price : int 
-    longitude : float
-    latitude : float
+    size: int
+    price: int
+    longitude: float
+    latitude: float
 
     @classmethod
-    def from_dict(cls,d):
-        return cls(**d)
+    def from_dict(self, d):
+        return self(**d)
 
     def to_dict(self):
         return dataclasses.asdict(self)
--- rentomatic/use_cases/room_list.py
+++ rentomatic/use_cases/room_list.py
@@ -1,3 +1,16 @@
-def room_list_use_case(repo):
-    rooms = repo.list()
-    return rooms
+from rentomatic.responses import (
+    ResponseSuccess,
+    ResponseFailure,
+    ResponseTypes,
+    build_response_from_invalid_request,
+)
+
+
+def room_list_use_case(repo, request):
+    if not request:
+        return build_response_from_invalid_request(request)
+    try:
+        rooms = repo.list(filters=request.filters)
+        return ResponseSuccess(rooms)
+    except Exception as exc:
+        return ResponseFailure(ResponseTypes.SYSTEM_ERROR, exc)
